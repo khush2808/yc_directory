e198e42ae625638b251b22d6bb6230125bd29bad
Khush Shah <khushshah675@gmail.com>
2025-01-20 02:38:15 +0530

    (feature): done with auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     <>
 		<section className ="pink_container">
 		<h1 className = "heading">Pitch Your Startup, <br /> Connect With Entreprenurs		</h1>
-
+<p className = "sub-heading !max-w-3xl">
+	Submit Ideas,Vote on Pitches, and Get Noticed in Virtual Competitions.	
+</p>
 		</section>
 		
 			
