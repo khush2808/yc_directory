9abe34c6f798c05ae6031f4b9464cb6c98549040
Khush Shah <khushshah675@gmail.com>
2025-01-20 02:30:31 +0530

    Landing page created

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+		<section className ="pink_container">
+		<h1 className = "heading">Pitch Your Startup, <br /> Connect With Entreprenurs		</h1>
+
+		</section>
+		
+			
+		</>
+  );
+}
